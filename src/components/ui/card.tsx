3da99c25bcdf7cd64934a7eb372c16f31555748c
CaterinaBu <109290102+CaterinaBu@users.noreply.github.com>
2025-09-29 21:01:34 +0700

    Create card.tsx

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+export function Card({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={"rounded-lg border border-gray-200 bg-white " + className} {...props} />;
+}
+export function CardContent({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={"p-4 " + className} {...props} />;
+}
