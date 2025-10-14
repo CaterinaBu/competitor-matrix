5c3705f52ac58c564346938064a21cb3c5ce291a
CaterinaBu <109290102+CaterinaBu@users.noreply.github.com>
2025-10-14 09:50:43 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// ЗАМЕНИ "competitor-matrix" на точное имя твоего репо
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: { "@": "/src" }
-  }
-});
+  base: '/competitor-matrix/',
+})
