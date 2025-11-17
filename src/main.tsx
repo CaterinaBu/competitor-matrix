f3032cc6afe2112ead5e3ea4bc39a6d8afb06adc
CaterinaBu <109290102+CaterinaBu@users.noreply.github.com>
2025-11-17 23:03:43 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import CompetitorMatrix from "./CompetitorMatrix";
+import { PasswordGate } from "./PasswordGate";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <CompetitorMatrix />
+    <PasswordGate>
+      <CompetitorMatrix />
+    </PasswordGate>
   </React.StrictMode>
 );
